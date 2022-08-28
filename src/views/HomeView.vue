7d4e958a7cd36d8f96b38235964dac5e509594ed
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2022-08-28 10:59:11 +0800

    another test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App (CARLO SEGOVIA)"/>
+    <HelloWorld msg="OKIE BOSKU"/>
   </div>
 </template>
 
